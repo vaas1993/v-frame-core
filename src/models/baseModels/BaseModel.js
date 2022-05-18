e67b09cc6e5a59ccec7b74ed2662ca856d786d70
vshen <1xfan1983@gmail.com>
2022-05-18 14:45:15 +0800

    Model增加事件响应机制

--- src/models/baseModels/BaseModel.js
+++ src/models/baseModels/BaseModel.js
@@ -235,6 +235,7 @@ export default class BaseModel {
             'fieldLabels',
             'response',
             '$unique',
+            '$events',
         ].indexOf(field) === -1 && (
             Object.prototype.hasOwnProperty.call(this, field)
             || this.formatConfig[field]
@@ -344,4 +345,52 @@ export default class BaseModel {
         model.setSources(ObjectHelper.copy(this.getSources()))
         return model
     }
+
+    /**
+     * 监听中的事件列表
+     * @type {object<array>}
+     */
+    $events = {}
+
+    /**
+     * 添加事件监听器
+     * @param {string} eventName 事件名称
+     * @param {function} callback 事件处理函数
+     * @param {boolean} once 是否只响应一次
+     */
+    $on(eventName, callback, once = false) {
+        this.$events[eventName] = this.$events[eventName] || []
+        this.$events[eventName].push({
+            once,
+            callback
+        })
+    }
+
+    /**
+     * 移除某个事件
+     * @param {string} eventName 事件名称
+     * @param {function} callback 事件处理函数
+     */
+    $off(eventName, callback) {
+        let list = (this.$events[eventName] || []).map(item => item.callback)
+        let index = list.indexOf(callback)
+        if( index !== -1 ) {
+            this.$events[eventName].splice(index, 1)
+        }
+    }
+
+    /**
+     * 触发事件
+     * @param {string} eventName 事件名称
+     * @param {*} params 事件参数
+     */
+    $emit(eventName, params) {
+        (this.$events[eventName] || []).map(item => {
+            item.callback(params)
+            if( item.once ) {
+                this.$off(eventName, item.callback)
+            }
+        })
+    }
+
 }
